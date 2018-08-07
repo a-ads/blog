875a63f8922d6fbef801fa6d81b0841fa3dca219
leo <leoplod1990@gmail.com>
2018-08-07 17:17:27 +0300

    Update blog preview

--- src/components/blog-preview.js
+++ src/components/blog-preview.js
@@ -35,6 +35,7 @@ export default class extends React.Component {
             previousLabel={'<'}
             nextLabel={'>'}
             disabledClassName={'--disabled'}
+            disableInitialCallback={true}
           />
         </div>
       </div>
