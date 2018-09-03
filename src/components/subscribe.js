14cc067158951bf1b8182c1132a20ae661e57762
leo <leoplod1990@gmail.com>
2018-09-03 15:22:15 +0300

    Update subscribe component

--- src/components/subscribe.js
+++ src/components/subscribe.js
@@ -3,46 +3,108 @@ import React from 'react'
 export default class extends React.Component {
   constructor (props) {
     super(props)
+    
+    this.state = {
+      email: '',
+      emailSent: false
+    }
+
+    this.onEmailInputChange = this.onEmailInputChange.bind(this)
+    this.onSubmitClick = this.onSubmitClick.bind(this)
   }
 
   render() {
     if (this.props.mobile) {
-      return (<SubscribeMobile />)
+      return this.renderSubscribeMobile()
     } else {
-      return (<Subscribe />)
+      return this.renderSubscribe()
     }
   }
-}
 
-const Subscribe = () => (
-  <div className='c-subscribe'>
-    <div className='c-subscribe__container'> 
-      <form>
-        <div className='c-subscribe__input-wrapper'>
-          <input type='text' placeholder='Enter email to get weekly newsletter...'/>
-        </div>
-        <div className='c-subscribe__submit-wrapper'>
-          <input type='submit' value='Subscribe'/>
-        </div>
-      </form>
-    </div>
-  </div>
-)
-
-const SubscribeMobile = () => (
-  <div className='c-subscribe c-subscribe--mobile'> 
-    <div className='c-subscribe__wrapper'>
-      <div className='c-subscribe__title'>Subscribe to our newsletter</div>
-      <div className='c-subscribe__container'>
-        <form>
-          <div className='c-subscribe__input-wrapper'>
-            <input type='text' placeholder='Enter your email'/>
+  renderSubscribe() {
+    return(
+      <div className='c-subscribe'>
+        {!this.isEmailSent() ? 
+          <div className='c-subscribe__container'> 
+            <form>
+              <div className='c-subscribe__input-wrapper'>
+                {this.renderEmailInput()}
+              </div>
+              <div className='c-subscribe__submit-wrapper'>
+                {this.renderSubmit({value: 'Subscribe'})}
+              </div>
+            </form>  
           </div>
-          <div className='c-subscribe__submit-wrapper'>
-            <input type='submit' value=''/>
+        :
+          <div className='c-subscribe__message-is-sent'>
+            <p>Thanks! Please, check out your email to confirm subscription.</p>
           </div>
-        </form>
+        }
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+
+  renderSubscribeMobile() {
+    return(
+      <div className='c-subscribe c-subscribe--mobile'> 
+        <div className='c-subscribe__wrapper'>
+          <div className='c-subscribe__title'>Subscribe to our newsletter</div>
+          <div className='c-subscribe__container'>
+            <form>
+              <div className='c-subscribe__input-wrapper'>
+                {this.renderEmailInput()}
+              </div>
+              <div className='c-subscribe__submit-wrapper'>
+                {this.renderSubmit()}
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  isEmailSent() {
+    return this.state.emailSent
+  }
+
+  renderEmailInput() {
+    return(
+      <input 
+        type='text' 
+        placeholder='Enter email to get weekly newsletter...'
+        onChange={this.onEmailInputChange}
+        value={this.state.email}
+      />
+    )
+  }
+
+  onEmailInputChange(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  renderSubmit(attrs = {
+    value: ''
+  }) {
+    const {
+      value,
+      ...rest
+    } = attrs
+
+    return(
+      <input type='submit' value={value} onClick={this.onSubmitClick} {...rest} />
+    )
+  }
+
+  onSubmitClick(event) {
+    event.preventDefault()
+    this.setState({
+      emailSent: true
+    })
+  }
+
+  
+
+}
