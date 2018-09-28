def688172b340d0eb35971b8ebce3c20166eff9c
leo <leoplod1990@gmail.com>
2018-09-28 13:24:10 +0300

    Update styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className='c-footer'> 
     <div className='c-footer__container l-container'> 
-      <div className='c-footer__copyright'>© A-ADS 2011—{new Date().getFullYear()}</div>
+      <div className='c-footer__links'><a href='/about/'>About A-ADS</a></div>
       <div className='c-footer__social-buttons'>
         <a href='#'><img src='/images/fb.svg' alt=''/></a>
         <a href='#'><img src='/images/twitter.svg' alt=''/></a>
@@ -11,7 +11,7 @@ export default () => (
         <a href='#'><img src='/images/reddit.svg' alt=''/></a>
         <a href='#'><img src='/images/btc.svg' alt=''/></a>
       </div>
-      <div className='c-footer__links'><a href='/about/'>About A-ADS</a><a href='/contacts/'>Contacts</a></div>
+      <div className='c-footer__copyright'>© A-ADS 2011—{new Date().getFullYear()}</div>
     </div>
   </div>
 )
