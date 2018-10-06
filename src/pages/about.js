5b5b36ceb28cde3eeef633524095d4c6228e1260
leo <leoplod1990@gmail.com>
2018-10-06 15:49:27 +0300

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,26 +1,44 @@
 import React from 'react'
-import AadsServices from '../components/aads-services'
 import Layout from '../layouts/index'
 
 export default () => (
   <Layout>
-    <article className='c-blog-article'>
-      <section className='c-blog-article__header'>
-        <div className='l-container'>
-          <h1 className='c-blog-article__title' style={{marginBottom: 0}}>
-            About
+    <article className='c-blog-post c-blog-post--static-page'>
+      <div className='l-blog-post-container'>
+        <section className='c-blog-post__header'>
+          <h1 className='c-blog-post__title'>
+            About A-ADS
           </h1>
-        </div>
-      </section>
-
-      <section className='c-blog-article__body'>
-        <div className='c-blog-article__body__container l-container'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-          <p>&nbsp;</p>
-        </div>
-      </section>
+        </section>
+        <section className='c-blog-post__body'>
+          <div className='c-blog-post__body__container'>
+            <p>
+              <strong>A-ADS</strong> (Anonymous Ads) is a bitcoin online advertising 
+              network that doesn’t collect personal data.
+            </p>
+            <p>
+              We believe there is a growing market for alternative 
+              online advertising that is efficient for advertisers while 
+              preserving users' privacy. That is why our ads neither 
+              contain scripts nor use cookies and they can be safely 
+              embedded anywhere.
+            </p>
+            <p>
+              Our scalable infrastructure comprises dozens of servers 
+              which deliver circa <strong>119</strong> million ad impressions a day to 
+              visitors from all over the world.
+            </p>
+            <p>
+              A-ADS offers transparent statistics, automated bitcoin 
+              transactions, and diverse traffic from thousands of sites 
+              and applications. It takes less than a minute to create 
+              a new advertising campaign. No registration required,&nbsp;
+              <a href='https://a-ads.com/campaigns/new' target='_blank'>try it yourself!</a>
+              <br /><br />
+            </p>
+          </div>
+        </section>
+      </div>
     </article>
-    <AadsServices />
   </Layout>
 )
