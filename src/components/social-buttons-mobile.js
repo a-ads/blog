3bc389556b95dc6a13fff2d9687b6ae0a31f0de2
leo <leoplod1990@gmail.com>
2018-11-14 14:54:14 +0300

    Fix sharing buttons

--- src/components/social-buttons-mobile.js
+++ src/components/social-buttons-mobile.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SocialButtonsDesktop from './social-buttons-desktop'
+import { withPrefix } from 'gatsby'
 
 export default class extends SocialButtonsDesktop {
   render() {
@@ -7,11 +8,11 @@ export default class extends SocialButtonsDesktop {
       <div className='c-soc-share --mobile'>
         <div className='c-soc-share__container'>
           <a className='c-soc-share__item --facebook' onClick={this.onFacebookButtonClick}>
-            <div className='c-soc-share__item-pic'><img src='/images/facebook_blue.svg' alt='fb'/></div>
+            <div className='c-soc-share__item-pic'><img src={withPrefix('/images/facebook_blue.svg')} alt='fb'/></div>
           </a>
 
           <a className='c-soc-share__item --twitter' onClick={this.onTwitterButtonClick}>
-            <div className='c-soc-share__item-pic'><img src='/images/twitter_blue.svg' alt='twitter'/></div>
+            <div className='c-soc-share__item-pic'><img src={withPrefix('/images/twitter_blue.svg')} alt='twitter'/></div>
           </a>
 
           {/* <a className='c-soc-share__item --medium' href='#'>
