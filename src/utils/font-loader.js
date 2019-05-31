cda1cfd5ba2d6aa88c2f6c9290e2e9f74dfd079f
Leonard <leo52-70@yandex.com>
2019-05-31 17:23:42 +0400

    fix

--- src/utils/font-loader.js
+++ src/utils/font-loader.js
@@ -1,13 +1,15 @@
 import eventEmitter from './event-emitter'
-import WebFont from 'webfontloader'
 import { FONTS_LOADED } from '../constants'
 
-WebFont.load({
-  google: {
-    families: ['Open Sans:400,600,700', 'Noto Sans:400,600,700']
-  },
-  active: () => {
-    eventEmitter.emit(FONTS_LOADED)
-  },
-  timeout: 2000
-})
+if (typeof window !== `undefined`) {
+  const WebFont = require('webfontloader')
+  WebFont.load({
+    google: {
+      families: ['Open Sans:400,600,700', 'Noto Sans:400,600,700']
+    },
+    active: () => {
+      eventEmitter.emit(FONTS_LOADED)
+    },
+    timeout: 2000
+  })
+}
