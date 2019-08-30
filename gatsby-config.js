be9000a4658de87fd406b123df059632c4468e28
Leonard <leo52-70@yandex.com>
2019-08-30 15:29:07 +0400

    Fix rss generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
+const PATH_PREFIX = '/blog';
+
 module.exports = {
-  pathPrefix: '/blog',
+  pathPrefix: PATH_PREFIX,
   siteMetadata: {
     title: 'A-ADS Blog',
     siteUrl: 'https://a-ads.com'
@@ -66,8 +68,9 @@ module.exports = {
         head: true,
       }
     },
+
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: `gatsby-plugin-feed`,
       options: {
         query: `
           {
@@ -80,7 +83,53 @@ module.exports = {
             }
           }
         `,
-      }
-    }
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + PATH_PREFIX + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + PATH_PREFIX + edge.node.fields.slug,
+                  custom_elements: [{
+                    "content:encoded": edge.node.html
+                  }]
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                      html
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+
+            // match: "^/blog/",
+          },
+        ],
+      },
+    },
+
   ],
 };
