0f41ddbc69310c90198d924b7cf08fd0a2e15fa3
aleksandr <shubin4433@gmail.com>
2023-07-06 08:44:45 +0000

    Merge branch '3134-noopener_nofollow' into staging
    # Conflicts:
    #	src/components/SocialButton.tsx

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -93,6 +93,7 @@ export type SocialButtonProps = {
   withText?: boolean
   customText?: string
   className?: string
+  title?: string
   [key: string]: unknown
 }
 
@@ -101,6 +102,7 @@ const SocialButton: React.FC<SocialButtonProps> = ({
   withText,
   customText,
   className,
+  title,
   ...props
 }) => {
   const {
@@ -116,9 +118,10 @@ const SocialButton: React.FC<SocialButtonProps> = ({
       ghost
       to={to}
       target='_blank'
+      title={title}
       rel='noopener nofollow'
       className={cn(className, {
-        'body-3 gap-2 font-primary': withText,
+        'body-3 gap-2 font-primary hover-social': withText,
       })}
       aria-label='Social media button'
       {...props}
