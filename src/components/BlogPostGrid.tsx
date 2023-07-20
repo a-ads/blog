0b7a5bc32fbadddf8e667b161d2de774d00acf05
aleksandr <shubin4433@gmail.com>
2023-07-20 16:59:49 +0700

    231-fix_pagination - more articles and add in tag a href

--- src/components/BlogPostGrid.tsx
+++ src/components/BlogPostGrid.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react'
 import cn from 'classnames'
 import { uniqueId } from 'lodash-es'
 import { Card } from '@components'
-import Pagination from './ui/pagination'
 import { Helmet } from 'react-helmet'
+import { navigate } from 'gatsby'
 import { useLocation } from '@reach/router'
+import Pagination from './ui/pagination'
 
 type BlogPostGridProps = {
   posts: BlogPostCard[]
@@ -19,7 +20,7 @@ type BlogPostGridProps = {
 
 const BlogPostGrid = ({
   posts = [],
-  amount = 5,
+  amount = 20,
   canLoadMore,
   span = [0],
   className,
@@ -28,68 +29,89 @@ const BlogPostGrid = ({
   setBlogPostGrid,
 }: BlogPostGridProps) => {
   const location = useLocation()
-  const queryParams = new URLSearchParams(location.search)
-  const initialPage = parseInt(queryParams.get('page') || '1', 10)
-
-  const [currentPage, setCurrentPage] = useState(initialPage - 1)
-  const [canonicalLink, setCanonicalLink] = useState('')
-  const [currentPageItems, setCurrentPageItems] = useState(() => {
-    const currentPageParam = queryParams.get('page') || '1'
-
-    const currentPage = currentPageParam
-      ? parseInt(currentPageParam, 10) - 1
-      : 0
-    const offset = currentPage * amount
-    return posts.slice(offset, offset + amount)
-  })
-
-  if (posts.length <= 0) {
-    return null
-  }
-
-  const pageCount = Math.ceil(posts.length / amount)
+  const [currentBlogs, setCurrentBlogs] = useState<BlogPostCard[]>([])
+  const [canonicalLink, setCanonicalLink] = useState(
+    `${location.origin}${location.pathname}`
+  )
+  const searchParams = new URLSearchParams(location.search)
+  const currentPage = parseInt(searchParams.get('page') as string) || 1
 
   useEffect(() => {
-    const currentPageParam = queryParams.get('page')
-    const currentPage = currentPageParam
-      ? parseInt(currentPageParam, 10) - 1
-      : 0
-    const offset = currentPage * amount
-    setCurrentPageItems(posts.slice(offset, offset + amount))
-  }, [])
+    const indexOfLastBlog = currentPage * amount
+    const indexOfFirstBlog = indexOfLastBlog - amount
 
-  const handlePageChange = (selectedPage: { selected: number }) => {
-    setCurrentPageItems(() => {
-      const offset = selectedPage.selected * amount
-      return posts.slice(offset, offset + amount)
-    })
+    if (posts.length <= amount) {
+      setCurrentBlogs(posts)
+    } else {
+      setCurrentBlogs(posts.slice(indexOfFirstBlog, indexOfLastBlog))
+    }
+  }, [currentPage, posts])
 
-    if (blogPostGrid) {
-      queryParams.set('page', String(selectedPage.selected + 1))
+  const totalPages = Math.ceil(posts.length / amount)
+  const maxDisplayPages = 5
 
-      if (typeof window !== 'undefined') {
-        const updatedUrl = `${location.pathname}?${queryParams.toString()}`
-        window.history.pushState(null, '', updatedUrl)
+  const pageNumbers = Array.from(
+    { length: Math.ceil(posts.length / amount) },
+    (_, index) => index + 1
+  )
+
+  const displayPageNumbers = (() => {
+    if (totalPages <= maxDisplayPages) {
+      return Array.from({ length: totalPages }, (_, index) => index + 1)
+    } else {
+      const middlePage = Math.floor(maxDisplayPages / 2)
+      if (currentPage <= middlePage) {
+        return Array.from(
+          { length: maxDisplayPages - 1 },
+          (_, index) => index + 1
+        )
+      } else if (currentPage >= totalPages - middlePage) {
+        return Array.from(
+          { length: maxDisplayPages - 1 },
+          (_, index) => totalPages - maxDisplayPages + 2 + index
+        )
+      } else {
+        return Array.from(
+          { length: maxDisplayPages - 2 },
+          (_, index) => currentPage - middlePage + index
+        )
       }
     }
+  })()
 
-    const updatedCanonicalLink = `${location.origin}${location.pathname}?page=${
-      selectedPage.selected + 1
-    }`
-    setCanonicalLink(updatedCanonicalLink)
-    setCurrentPage(selectedPage.selected + 1)
+  const goToPage = (page: number) => {
+    if (page === 1) {
+      navigate(`${location.origin}${location.pathname}`, { replace: true })
+    } else if (page >= 1 && page <= totalPages) {
+      navigate(`${location.origin}${location.pathname}?page=${page}`)
+    }
   }
 
   useEffect(() => {
     if (setBlogPostGrid) {
       setBlogPostGrid(true)
+    }
 
-      if (typeof window !== 'undefined') {
-        const linkElement = document.querySelector('link[rel="canonical"]')
-        if (linkElement) {
-          linkElement.setAttribute('href', canonicalLink)
-        }
+    if (currentPage > 1) {
+      setCanonicalLink(
+        `${location.origin}${location.pathname}?page=${currentPage}`
+      )
+    } else {
+      setCanonicalLink(`${location.origin}${location.pathname}`)
+    }
+
+    if (typeof window !== 'undefined') {
+      const linkElement = document.querySelector('link[rel="canonical"]')
+      console.log(linkElement, 'linkElement')
+      if (linkElement) {
+        linkElement.setAttribute('href', canonicalLink)
       }
+    }
+  }, [currentPage, canonicalLink])
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.scrollTo(0, 0)
     }
   }, [currentPage])
 
@@ -104,8 +126,8 @@ const BlogPostGrid = ({
           className
         )}
       >
-        {currentPageItems &&
-          currentPageItems?.map((post, i) => (
+        {currentBlogs &&
+          currentBlogs?.map((post, i) => (
             <Card
               key={uniqueId()}
               className={cn('mb-8 phone:mb-0 hover-card', {
@@ -116,13 +138,13 @@ const BlogPostGrid = ({
             />
           ))}
       </div>
-      {canLoadMore && (
-        <Pagination
-          onPageChange={handlePageChange}
-          pageCount={pageCount}
-          initialPage={initialPage - 1}
-        />
-      )}
+      <Pagination
+        goToPage={goToPage}
+        displayPageNumbers={displayPageNumbers}
+        canLoadMore={canLoadMore}
+        pageNumbers={pageNumbers}
+        currentPage={currentPage}
+      />
     </>
   )
 }
--- src/components/ui/pagination/index.tsx
+++ src/components/ui/pagination/index.tsx
@@ -1,32 +1,62 @@
-import React, { memo } from 'react'
-import ReactPaginate from 'react-paginate'
+import React from 'react'
 import './pagination.css'
+import { Link } from 'gatsby'
+import { useLocation } from '@reach/router'
 
-interface IPaginationProps {
-  onPageChange: (selectedItem: { selected: number }) => void
-  pageCount: number
-  initialPage: number
+interface IPropsPagination {
+  goToPage: (page: number) => void
+  displayPageNumbers: number[]
+  canLoadMore?: boolean
+  pageNumbers: number[]
+  currentPage: number
 }
 
 const Pagination = ({
-  onPageChange,
-  pageCount,
-  initialPage,
-}: IPaginationProps) => {
+  goToPage,
+  displayPageNumbers,
+  canLoadMore,
+  pageNumbers,
+  currentPage,
+}: IPropsPagination) => {
+  const location = useLocation()
   return (
-    <ReactPaginate
-      initialPage={initialPage}
-      pageCount={pageCount}
-      marginPagesDisplayed={2}
-      pageRangeDisplayed={2}
-      onPageChange={onPageChange}
-      containerClassName={'pagination'}
-      activeClassName={'active'}
-      nextLabel=' >'
-      previousLabel='< '
-      breakLabel='...'
-    />
+    <>
+      {displayPageNumbers.length > 1 && (
+        <ul className='pagination'>
+          <button
+            onClick={() => goToPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            {'<'}
+          </button>
+          {canLoadMore &&
+            displayPageNumbers.map((number: any) => {
+              return (
+                <li className={currentPage === number ? 'active' : ''}>
+                  <Link
+                    to={
+                      number === 1
+                        ? `${location.origin}${location.pathname}`
+                        : `${location.origin}${location.pathname}?page=${number}`
+                    }
+                    key={number}
+                    className={currentPage === number ? 'active' : ''}
+                  >
+                    {number}
+                  </Link>
+                </li>
+              )
+            })}
+          <button
+            onClick={() => goToPage(currentPage + 1)}
+            disabled={currentPage === pageNumbers.length}
+          >
+            {'>'}
+          </button>
+        </ul>
+      )}
+    </>
   )
 }
 
-export default memo(Pagination)
+export default Pagination
