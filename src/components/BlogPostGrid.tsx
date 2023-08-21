39582bf136ad2eca81889bd0662c175c48614edb
aleksandr <shubin4433@gmail.com>
2023-08-21 15:15:42 +0700

    231-fix_pagination - fix href in pagination

--- src/components/BlogPostGrid.tsx
+++ src/components/BlogPostGrid.tsx
@@ -108,7 +108,7 @@ const BlogPostGrid = ({
     }
   }, [currentPage])
 
-  const hrefBuilder = () => {
+  const hrefBuilder = (currentPage: number) => {
     if (currentPage === 1) {
       return `${location.origin}${location.pathname}`
     } else {
@@ -145,6 +145,7 @@ const BlogPostGrid = ({
           pageCount={pageCount}
           initialPage={initialPage - 1}
           hrefBuilder={hrefBuilder}
+          currentPage={currentPage}
         />
       )}
     </>
