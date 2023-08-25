835e73f644b8d32b1886e160c840d61b287ce863
aleksandr <shubin4433@gmail.com>
2023-08-25 10:10:11 +0000

    Merge branch '3274-fix_displaying_articles' into staging

--- src/templates/BlogPostTemplate.tsx
+++ src/templates/BlogPostTemplate.tsx
@@ -12,13 +12,36 @@ export function Head({ pageContext: { post, author } }) {
   return (
     <Seo title={post.meta_title} description={post.meta_description}>
       {post.json_ld ? (
-        <script
-          type='application/ld+json'
-          dangerouslySetInnerHTML={{ __html: post.json_ld }}
-        />
+        <script type='application/ld+json'>
+          {`[${post.json_ld},
+          {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [{
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Blog",
+            "item": "https://a-ads.com/blog/"
+            },{
+            "@type": "ListItem",
+            "position": 2,
+            "name": "${post.category_top_level[0]}",
+            "item": "https://a-ads.com/blog/categories/${
+              post.category_top_level[0]
+            }/"
+            },{
+            "@type": "ListItem",
+            "position": 3,
+            "name": ${post?.category_second_level?.[0] || ''}
+            "item": "https://a-ads.com/blog/categories/${
+              post?.category_second_level?.[0] || ''
+            }/"
+            }]
+          }]`}
+        </script>
       ) : (
         <script type='application/ld+json'>
-          {`{
+          {`[{
             "@context": "https://schema.org",
             "@type": "BlogPosting",
             "headline": "${post.meta_title}",
@@ -33,7 +56,31 @@ export function Head({ pageContext: { post, author } }) {
               post.thumbnail?.childImageSharp?.gatsbyImageData?.images?.fallback
                 ?.src
             )}"]
-          }`}
+          },
+          {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          "itemListElement": [{
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Blog",
+            "item": "https://a-ads.com/blog/"
+            },{
+            "@type": "ListItem",
+            "position": 2,
+            "name": "${post.category_top_level[0]}",
+            "item": "https://a-ads.com/blog/categories/${
+              post.category_top_level[0]
+            }/"
+            },{
+            "@type": "ListItem",
+            "position": 3,
+            "name": ${post.category_second_level?.[0]}
+            "item": "https://a-ads.com/blog/categories/${
+              post.category_second_level?.[0]
+            }/"
+            }]
+          }]`}
         </script>
       )}
     </Seo>
