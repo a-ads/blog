087d052d083696c5ff87f4405da133d17fcce129
aleksandr <shubin4433@gmail.com>
2023-08-25 10:10:53 +0000

    staging - version 9

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -294,7 +294,7 @@ const Footer = () => {
           </p>
         ))}
       </div>
-      <div className='text-center text-gray-400 text-xs'>version: 8</div>
+      <div className='text-center text-gray-400 text-xs'>version: 9</div>
     </footer>
   )
 }
