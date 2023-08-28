7bb5e567c226338b00678383e263ce1d33a5547a
aleksandr <shubin4433@gmail.com>
2023-08-28 09:48:36 +0000

    staging - version: 12

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -294,7 +294,7 @@ const Footer = () => {
           </p>
         ))}
       </div>
-      <div className='text-center text-gray-400 text-xs'>version: 11</div>
+      <div className='text-center text-gray-400 text-xs'>version: 12</div>
     </footer>
   )
 }
