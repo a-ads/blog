f804d833d4ec4e3b4b07066bd2ec92ed85cb3b00
aleksandr <shubin4433@gmail.com>
2023-08-30 16:38:17 +0700

    ыефпштп - version: 18

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -294,7 +294,7 @@ const Footer = () => {
           </p>
         ))}
       </div>
-      <div className='text-center text-gray-400 text-xs'>version: 17</div>
+      <div className='text-center text-gray-400 text-xs'>version: 18</div>
     </footer>
   )
 }
