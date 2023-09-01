1ee39ed0ce74cc6dd79f95108d39f7beb4e0835e
aleksandr <shubin4433@gmail.com>
2023-09-01 06:23:52 +0000

    staging - temp commit

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -294,7 +294,7 @@ const Footer = () => {
           </p>
         ))}
       </div>
-      <div className='text-center text-gray-400 text-xs'>version: 19</div>
+      <div className='text-center text-gray-400 text-xs'>version: 20</div>
     </footer>
   )
 }
