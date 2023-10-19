152a18384991125779a36ce1f3824bff5b92dc0e
aleksandr <shubin4433@gmail.com>
2023-10-19 16:59:27 +0700

    staging - change version

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -294,7 +294,7 @@ const Footer = () => {
           </p>
         ))}
       </div>
-      <div className='text-center text-gray-400 text-xs'>version: 23</div>
+      <div className='text-center text-gray-400 text-xs'>version: 24</div>
     </footer>
   )
 }
