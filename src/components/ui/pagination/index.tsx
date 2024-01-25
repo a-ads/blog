95c40f3ca2ef6e6af2b44d1b74e4206c594203de
aleksandr <shubin4433@gmail.com>
2024-01-25 19:31:06 +0700

    staging - temp commit

--- src/components/ui/pagination/index.tsx
+++ src/components/ui/pagination/index.tsx
@@ -37,7 +37,7 @@ const Pagination = ({
                     to={
                       number === 1
                         ? `${location.pathname}`
-                        : `${location.pathname}?page=${number}`
+                        : `?page=${number}`
                     }
                     key={number}
                     className={currentPage === number ? 'active' : ''}
