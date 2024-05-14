f9b6439fe2b1ef3985da8b24b105fd7050cf4485
aleksandr <shubin4433@gmail.com>
2024-05-14 16:54:31 +0200

    staging - WIP

--- src/components/ui/pagination/index.tsx
+++ src/components/ui/pagination/index.tsx
@@ -1,65 +1,77 @@
-import React from 'react'
-import './pagination.css'
-import { Link } from 'gatsby'
+import React from 'react';
+import './pagination.css';
+import { Link } from 'gatsby';
 import { useLocation } from '@reach/router';
 
 interface IPropsPagination {
-  goToPage: (page: number) => void
-  displayPageNumbers: number[]
-  canLoadMore?: boolean
-  pageNumbers: number[]
-  currentPage: number
+  goToPage: (page: number) => void;
+  displayPageNumbers: number[];
+  canLoadMore?: boolean;
+  pageNumbers: number[];
+  currentPage: number;
 }
 
 const Pagination = ({
-  goToPage,
-  displayPageNumbers,
-  canLoadMore,
-  pageNumbers,
-  currentPage,
-}: IPropsPagination) => {
+                      goToPage,
+                      displayPageNumbers,
+                      canLoadMore,
+                      pageNumbers,
+                      currentPage,
+                    }: IPropsPagination) => {
   const location = useLocation();
 
   console.log(location, 'location');
+
+  const getPathForPage = (page: number) => {
+    if (page === 1) {
+      return location.pathname.replace(/\/index\d*\.html$/, '/') || '/';
+    } else {
+      return location.pathname.replace(/\/index\d*\.html$/, '') + `/index${page}.html`;
+    }
+  };
 
   return (
     <>
       {displayPageNumbers.length > 1 && (
         <ul className='pagination'>
-          <button
-            onClick={() => goToPage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            {'<'}
-          </button>
-          {canLoadMore &&
-            displayPageNumbers.map((number: any) => {
-              return (
-                <li key={number} className={currentPage === number ? 'active' : ''}>
-                  <Link
-                    to={
-                      number === 1
-                        ? `/`
-                        : `${location.origin}${location.pathname}index${number - 1}.html` + `?page=${number}`
-                    }
-                    key={number}
-                    className={currentPage === number ? 'active' : ''}
-                  >
-                    {number}
-                  </Link>
-                </li>
-              )
-            })}
-          <button
-            onClick={() => goToPage(currentPage + 1)}
-            disabled={currentPage === pageNumbers.length}
-          >
-            {'>'}
-          </button>
+          <li>
+            <Link
+              to={getPathForPage(currentPage - 1)}
+              onClick={() => goToPage(currentPage - 1)}
+              className={currentPage === 1 ? 'disabled' : ''}
+            >
+              {'<'}
+            </Link>
+          </li>
+          {canLoadMore && displayPageNumbers.map((number: number) => {
+            const isCurrentPage = currentPage === number;
+            const pagePath = getPathForPage(number);
+
+            return (
+              <li key={number} className={isCurrentPage ? 'active' : ''}>
+                <Link
+                  to={pagePath}
+                  key={number}
+                  className={isCurrentPage ? 'active' : ''}
+                >
+                  {number}
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            <Link
+              to={getPathForPage(currentPage + 1)}
+              onClick={() => goToPage(currentPage + 1)}
+              className={currentPage === pageNumbers.length ? 'disabled' : ''}
+            >
+              {'>'}
+            </Link>
+          </li>
         </ul>
       )}
     </>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
