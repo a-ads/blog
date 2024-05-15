958c98b94b948b5228e032f759d3293e6570fd71
aleksandr <shubin4433@gmail.com>
2024-05-15 11:51:14 +0200

    staging - WIP

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -185,7 +185,7 @@ const Footer = () => {
                             <a className="copy__item" href='https://a-ads.com/terms_of_service'>Terms of Service</a>
                             <a className="copy__item" href='https://a-ads.com/privacy_policy'>Privacy Policy</a>
                         </div>
-                        <p>version 4</p>
+                        <p>version 5</p>
                     </div>
                 </div>
             </div>
